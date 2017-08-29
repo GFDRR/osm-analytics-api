864bf4ef8b2a6502f679a230a6b3db0580e97188
rrequero <rrequero@gmail.com>
2017-08-29 16:28:06 +0200

    add historic pbf

--- api/app/src/routes/pbf.router.js
+++ api/app/src/routes/pbf.router.js
@@ -19,9 +19,24 @@ class PbfRouter {
       return;
     }
   }
+
+  static async pbf(ctx) {
+    logger.info(`Obtaining tile of year ${ctx.params.year} and layer: ${ctx.params.layer} - z: ${ctx.params.z}; x: ${ctx.params.x}; y: ${ctx.params.y}`)
+    try {
+      const data = await tileService.getTileNotParse(ctx.params.z, ctx.params.x, ctx.params.y, `${ctx.params.layer}-${ctx.params-year}`);
+      ctx.body = data.tile;
+      ctx.set(data.headers);
+    } catch(err) {
+      logger.error('Tile does not exist', err);
+      ctx.status = 404;
+      ctx.body = 'Tile does not exist';
+      return;
+    }
+  }
 }
 
 router.get('/:layer/:z/:x/:y.pbf', PbfRouter.pbf);
+router.get('/:year/:layer/:z/:x/:y.pbf', PbfRouter.historic);
 
 module.exports = {
   router,
